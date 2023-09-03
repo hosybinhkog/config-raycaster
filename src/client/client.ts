25e066ea42d562582eadb7ee4a487ef37c1cb33e
hobinh <hobinh@MacBook-Pro-cua-hobinh.local>
2023-09-03 20:21:15 +0700

    handle click create object save

--- src/client/client.ts
+++ src/client/client.ts
@@ -3,8 +3,8 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 
 const scene = new THREE.Scene()
 
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
-camera.position.z = 2
+const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000)
+camera.position.set(0, 6, 6)
 
 const renderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
@@ -12,14 +12,48 @@ document.body.appendChild(renderer.domElement)
 
 const controls = new OrbitControls(camera, renderer.domElement)
 
-const geometry = new THREE.BoxGeometry()
-const material = new THREE.MeshBasicMaterial({
-    color: 0x00ff00,
-    wireframe: true,
+const ambientLight = new THREE.AmbientLight(0xffffff, 1)
+scene.add(ambientLight)
+
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.8)
+scene.add(directionalLight)
+directionalLight.position.set(0, 50, 0)
+
+const axesHelper = new THREE.AxesHelper(20)
+scene.add(axesHelper)
+
+const gridHelper = new THREE.GridHelper(40, 40, 'skyblue', 'skyblue')
+scene.add(gridHelper)
+
+const mouse = new THREE.Vector2()
+const intersectionPoint = new THREE.Vector3()
+const planeNormal = new THREE.Vector3()
+const plane = new THREE.Plane()
+const raycaster = new THREE.Raycaster()
+
+window.addEventListener('mousemove', function (e) {
+    mouse.x = (e.clientX / window.innerWidth) * 2 - 1
+    mouse.y = -(e.clientY / window.innerHeight) * 2 + 1
+    planeNormal.copy(camera.position).normalize()
+    plane.setFromNormalAndCoplanarPoint(planeNormal, scene.position)
+    raycaster.setFromCamera(mouse, camera)
+    raycaster.ray.intersectPlane(plane, intersectionPoint)
 })
 
-const cube = new THREE.Mesh(geometry, material)
-scene.add(cube)
+window.addEventListener('click', function (e) {
+    const sphereGeo = new THREE.SphereGeometry(0.125, 30, 30)
+    const sphereMat = new THREE.MeshStandardMaterial({
+        color: 'skyblue',
+        metalness: 0,
+        roughness: 0,
+    })
+    const sphereMesh = new THREE.Mesh(sphereGeo, sphereMat)
+    scene.add(sphereMesh)
+    console.log(intersectionPoint)
+    const randomScale = Math.floor(Math.random() * 10)
+    sphereMesh.scale.set(randomScale, randomScale, randomScale)
+    sphereMesh.position.copy(intersectionPoint)
+})
 
 window.addEventListener('resize', onWindowResize, false)
 function onWindowResize() {
@@ -31,9 +65,6 @@ function onWindowResize() {
 
 function animate() {
     requestAnimationFrame(animate)
-
-    cube.rotation.x += 0.01
-    cube.rotation.y += 0.01
 
     controls.update()
 
